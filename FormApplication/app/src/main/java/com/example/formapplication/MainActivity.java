759375b90b13e79a0d1a6956b9def37984264977
Jake <83174584+Jake-h04@users.noreply.github.com>
2026-09-24 17:18:42 +0100

    Form Application Added

---
+++ FormApplication/app/src/main/java/com/example/formapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.formapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onSubmit(View view) {
+
+        TextView name = (TextView) findViewById(R.id.editName);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("Thank you, " + name.getText() + " For submitting the form!");
+    }
+}
